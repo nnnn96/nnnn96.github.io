bee87032310c4777cef8acc7694a453330c25733
niuzx <2422631371@qq.com>
2021-11-11 21:06:55 +0800

    提交到gitee

--- deploy.sh
+++ deploy.sh
@@ -31,15 +31,15 @@ git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
 # echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
-# if [ -z "$GITEE_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-#   codingUrl=git@gitee.com:nn0824/my-blog.git
-# else
-#   codingUrl=https://nn0824:${GITEE_TOKEN}@gitee.com/nn0824/my-blog.git
+ if [ -z "$GITEE_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+   codingUrl=git@gitee.com:nn0824/my-blog.git
+ else
+   codingUrl=https://nn0824:${GITEE_TOKEN}@gitee.com/nn0824/my-blog.git
    
-# fi
-# git add -A
-# git commit -m "${msg}"
-# git push -f $codingUrl master # 推送到coding
+ fi
+ git add -A
+ git commit -m "${msg}"
+ git push -f $codingUrl master # 推送到coding
 
 cd -
 rm -rf docs/.vuepress/dist
