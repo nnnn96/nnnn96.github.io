4cc84b7a047bcea94e0813e3c0e37cc16f87dd6a
nnnn96 <2422631371@qq.com>
2021-12-30 17:02:40 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around',
+  title: "Niuzx's blog",
+  description: '种一棵树最好是在十年前，其次是现在',
   // 注入到当前页面的 HTML <head> 中的标签
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
@@ -13,28 +13,28 @@ module.exports = {
     sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
     lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
 	nav:[
-      { text: '前端算法', link: '/algorithm/' }, // 内部链接 以docs为根目录
-      { text: '博客', link: 'http://obkoro1.com/' }, // 外部链接
+      { text: '算法', link: '/algorithm/' }, // 内部链接 以docs为根目录
       // 下拉列表
       {
-        text: 'GitHub',
+        text: '数据库',
         items: [
-          { text: 'GitHub地址', link: 'https://github.com/OBKoro1' },
-          {
-            text: '算法仓库',
-            link: 'https://github.com/OBKoro1/Brush_algorithm'
-          }
+		{ text: 'MySQL', link: '/mysql/' },
+		{ text: 'NoSQL数据库', 
+			items: [
+				{ text: 'Redis', link: '/redis/01.redis简介' }
+			] 
+		}
         ]
       }        
     ],
 	sidebar:{
         // docs文件夹下面的accumulate文件夹 文档中md文件 书写的位置(命名随意)
-        '/accumulate/': [
-            '/accumulate/', // accumulate文件夹的README.md 不是下拉框形式
+        '/redis/': [
+            '/redis/', // accumulate文件夹的README.md 不是下拉框形式
             {
-              title: '侧边栏下拉框的标题1',
+              title: 'Redis',
               children: [
-                '/accumulate/JS/test', // 以docs为根目录来查找文件 
+                '/redis/01.redis简介', // 以docs为根目录来查找文件 
                 // 上面地址查找的是：docs>accumulate>JS>test.md 文件
                 // 自动加.md 每个子选项的标题 是该md文件中的第一个h1/h2/h3标题
               ]
@@ -44,7 +44,7 @@ module.exports = {
           '/algorithm/': [
             '/algorithm/', 
             {
-              title: '第二组侧边栏下拉框的标题1',
+              title: '算法',
               children: [
                 '/algorithm/simple/test' 
               ]
